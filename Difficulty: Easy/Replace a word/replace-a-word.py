49c91c761b35a4b3c52ed2998738784878cfbb16
MUPPARAPU JUDSON ABHISHEK <155309843+JudsonAbhishek@users.noreply.github.com>
2025-02-22 20:18:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Replace a word/replace-a-word.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+class Solution:
+    def replaceAll (ob, S, oldW, newW):
+        return S.replace(oldW,newW)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#Initial Template for Python 3
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        S = input()
+        oldW = input()
+        newW = input()
+        
+        ob = Solution()
+        print(ob.replaceAll(S, oldW, newW))
+# } Driver Code Ends
